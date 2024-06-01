311dadc949753531aa1227da78be46858bb08872
user <user@pc.my.domain>
2024-06-01 11:38:32 +0300

    .

---
+++ .dots/.zshrc
@@ -0,0 +1,48 @@
+export ZSH="$HOME/.oh-my-zsh"
+
+#ZSH_THEME="agnoster"
+
+eval "$(starship init zsh)"
+
+plugins=(
+	git
+	sudo
+	web-search
+	python
+	pip
+	history-substring-search
+	colored-man-pages
+	zsh-autosuggestions
+	zsh-syntax-highlighting
+)
+
+source $ZSH/oh-my-zsh.sh
+
+
+
+alias p="python3"
+alias c="clear"
+alias j='jsonVer(){ cat "$@" | jq; unset -f jsonVer; }; jsonVer'
+alias ara='ara(){ find / -type f -name "$@" -print 2>/dev/null }; ara'
+alias md2pdf='md2pdf(){ pandoc -o "${@%%.*}.pdf" --template pdf_theme --listings --pdf-engine=xelatex --toc "$@"; unset -f md2pdf; }; md2pdf'
+
+# alias ipv4="nmcli device show | grep IP4.ADDRESS | head -1 | awk '{print $2}' | rev | cut -c 4- | rev"
+alias ipv4="nmcli device show | awk '/IP4.ADDRESS/{print \$2}' | cut -d'/' -f1 | head -1"
+
+# alias ipv6="nmcli device show | grep IP6.ADDRESS | head -1 | awk '{print $2}' | rev | cut -c 4- | rev"
+alias ipv6="nmcli device show | awk '/IP6.ADDRESS/{print \$2}' | cut -d'/' -f1 | head -1"
+
+# xinput --set-prop "Elan TrackPoint" "libinput Accel Speed" -0.7
+alias yt='yt-dlp -f "bestvideo[ext=mp4]+bestaudio[ext=m4a]/bestvideo+bestaudio" --merge-output-format mp4'
+alias mp3="yt-dlp -x --embed-thumbnail --audio-format mp3"
+
+# * mkv2mp4
+alias mkv2mp4='mkv2mp4(){ ffmpeg -v quiet -stats -i "$@" -c copy -c:a aac -movflags +faststart "${@%%.*}.mp4" }; mkv2mp4'
+
+# * GPG_KEY
+export GPG_TTY=$(tty)
+
+
+ * TR dil ayarları
+export LC_ALL=tr_TR.UTF-8
+export LANG=tr_TR.UTF-8
---
+++ alpine.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+# General
+doas apk add python-pip fastfetch android-tools rust npm git wget
+
+# Python
+pip3 install --break-system-packages -U pip yt-dlp setuptools wheel
+
+# Git
+git config --global user.email "root@localhost.localdomain"
+git config --global user.name "Root"
+git config --global credential.helper "cache --timeout=36000"
+
+# ZSH
+doas apk add zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+git clone https://github.com/zsh-users/zsh-completions.git ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+rm -rf ~/.zshrc && wget -O ~/.zshrc https://raw.githubusercontent.com/0x733/.dotfiles/main/.dots/.zshrc
+
+
+# Flatpak
+doas apk add flatpak 
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+flatpak remote-add --if-not-exists flathub-beta https://flathub.org/beta-repo/flathub-beta.flatpakrepo
+flatpak update && flatpak upgrade
+flatpak install flathub net.mullvad.MullvadBrowser
+flatpak install flathub com.brave.Browser
+flatpak install flathub io.freetubeapp.FreeTube
+flatpak install flathub io.github.mimbrero.WhatsAppDesktop
+flatpak install flathub com.mattjakeman.ExtensionManager
+flatpak install flathub com.github.ADBeveridge.Raider
+
+# Python Update
+python3 -m pip install --upgrade pip
+pip install pip-review --break-system-packages
+pip-review --local --interactive --break-system-packages
