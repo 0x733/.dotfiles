4b698d30f802e234b07bb0a103fc0b321f74d365
0x733 <98838267+0x733@users.noreply.github.com>
2024-06-19 18:31:46 +0300

    .

--- cachy.sh
+++ cachy.sh
@@ -38,7 +38,8 @@ install_packages() {
   echo "Ek paketler yükleniyor..."
   local packages=(
     "git" "wget" "curl" "xdg-user-dirs" "playerctl" "unzip" "zip"
-    "p7zip" "unrar" "tar" "rsync" "qt5ct" "kvantum"
+    "p7zip" "unrar" "tar" "rsync" "qt5ct" "kvantum" "gvfs" "gvfs-smb" "gvfs-mtp"
+    "gvfs-afc" "gvfs-goa" "gvfs-google" "gvfs-gphoto2" "gvfs-nfs" "gvfs-afc"
   )
   sudo pacman -Sy --noconfirm "${packages[@]}"
 
