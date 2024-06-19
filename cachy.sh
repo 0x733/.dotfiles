ab26202107fa21e7af491579c0575c522d3a00b5
0x733 <98838267+0x733@users.noreply.github.com>
2024-06-19 21:19:28 +0300

    .

--- cachy.sh
+++ cachy.sh
@@ -109,6 +109,8 @@ update_user_dirs() {
 
 setup_fonts() {
   echo "Yazı tipleri kuruluyor..."
+  local font_packages=("noto-fonts" "noto-fonts-cjk" "noto-fonts-emoji" "noto-fonts-extra")
+  sudo pacman -Sy --noconfirm "${font_packages[@]}"
   cp "$DOTFILES_DIR/.fonts.conf" "$HOME/"
   cp -r "$DOTFILES_DIR/.fonts" "$HOME/"
   cp -r "$DOTFILES_DIR/.config/fontconfig" "$HOME/.config/"
