d2901550f456f916b3f286eb2c98465fa151c93a
0x733 <98838267+0x733@users.noreply.github.com>
2024-06-19 20:29:33 +0300

    .

--- cachy.sh
+++ cachy.sh
@@ -52,7 +52,8 @@ install_pacman_apps() {
   echo "Pacman paketleri yükleniyor..."
   local packages=(
     "brave-bin" "mullvad-browser-bin" "onionshare"
-    "vlc" "dolphin-emu" "yt-dlp" "starship" "waydroid"
+    "vlc" "dolphin-emu" "yt-dlp" "starship" "waydroid" 
+    "chatgpt-desktop-bin"
   )
   sudo pacman -Sy --noconfirm "${packages[@]}"
 }
