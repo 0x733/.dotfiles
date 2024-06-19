9b5afa71ef162615de750a113f9fc7839ddfbb52
0x733 <98838267+0x733@users.noreply.github.com>
2024-06-19 22:52:23 +0300

    .

--- sddm.sh
+++ sddm.sh
@@ -10,7 +10,7 @@ log() {
 # Sistem güncelleme ve bağımlılıkların kurulumu
 install_dependencies() {
   echo "Gerekli bağımlılıklar yükleniyor..."
-  sudo pacman -Sy --noconfirm qt6-svg qt6-declarative
+  pacman -Sy --noconfirm qt6-svg qt6-declarative
 }
 
 # Geçici dosyaları temizleme fonksiyonu
@@ -22,19 +22,24 @@ cleanup() {
 # SDDM teması indirme ve kurulum
 install_sddm_theme() {
   echo "SDDM teması indiriliyor ve kuruluyor..."
-  wget -q --show-progress --progress=bar:force:noscroll -O /tmp/catppuccin-mocha.zip https://github.com/catppuccin/sddm/releases/download/v1.0.0/catppuccin-mocha.zip
-  unzip -q /tmp/catppuccin-mocha.zip -d /tmp
-  sudo mv /tmp/catppuccin-mocha /usr/share/sddm/themes/
+  sudo mkdir -p /usr/share/sddm/themes  # Tema dizini oluştur
+
+  # Temayı root dizinine indir ve kur
+  sudo wget -q --show-progress --progress=bar:force:noscroll -O /tmp/catppuccin-mocha.zip https://github.com/catppuccin/sddm/releases/download/v1.0.0/catppuccin-mocha.zip
+  sudo unzip -q /tmp/catppuccin-mocha.zip -d /usr/share/sddm/themes/
 }
 
 # SDDM yapılandırma dosyasına tema ekleme
 configure_sddm() {
   echo "SDDM yapılandırması güncelleniyor..."
-  sudo mkdir -p /etc
-  if grep -q "^\[Theme\]" /etc/sddm.conf; then
-    sudo sed -i '/^\[Theme\]/!b;n;c\Current=catppuccin-mocha' /etc/sddm.conf
+
+  # Varsayılan yapılandırma dosyasını kontrol et ve güncelle
+  sudo mkdir -p /etc/sddm.conf.d   # Yapılandırma dizini oluştur
+
+  if ! sudo grep -q "^\[Theme\]" /etc/sddm.conf.d/default.conf; then
+    echo -e "[Theme]\nCurrent=catppuccin-mocha" | sudo tee /etc/sddm.conf.d/default.conf
   else
-    echo -e "\n[Theme]\nCurrent=catppuccin-mocha" | sudo tee -a /etc/sddm.conf
+    sudo sed -i '/^\[Theme\]/!b;n;c\Current=catppuccin-mocha' /etc/sddm.conf.d/default.conf
   fi
 }
 
