cbf65ecd430697446ee70e2725128686360f6438
0x733 <98838267+0x733@users.noreply.github.com>
2024-06-19 22:41:47 +0300

    .

--- cachy.sh
+++ cachy.sh
@@ -83,16 +83,23 @@ setup_zsh() {
   sudo pacman -Sy --noconfirm --needed zsh
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
-  # ZSH için gerekli pluginlerin kurulumu
-  echo "ZSH pluginleri kuruluyor..."
-  git clone --depth 1 https://github.com/zsh-users/zsh-completions.git "${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions"
-  git clone --depth 1 https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
-  git clone --depth 1 https://github.com/zsh-users/zsh-autosuggestions.git "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
+  # ZSH_CUSTOM dizinini tanımlayın
+  ZSH_CUSTOM=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}
 
-  # .zshrc dosyasının indirilmesi
-  rm -rf ~/.zshrc && wget -O ~/.zshrc https://raw.githubusercontent.com/0x733/.dotfiles/main/.dots/.zshrc
+  # Eklenti dizinlerinin var olduğundan emin olun
+  mkdir -p $ZSH_CUSTOM/plugins/zsh-completions
+  mkdir -p $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+  mkdir -p $ZSH_CUSTOM/plugins/zsh-autosuggestions
 
-  # Kabuğu ZSH olarak ayarla
+  # Eklentileri klonlayın
+  git clone --depth 1 https://github.com/zsh-users/zsh-completions.git $ZSH_CUSTOM/plugins/zsh-completions
+  git clone --depth 1 https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+  git clone --depth 1 https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+
+  # .zshrc yapılandırma dosyasını indirin
+  rm -rf ~/.zshrc && wget -q --show-progress --progress=bar:force:noscroll -O ~/.zshrc https://raw.githubusercontent.com/0x733/.dotfiles/main/.dots/.zshrc
+
+  # ZSH kabuğunu varsayılan kabuk olarak ayarlayın
   chsh -s /bin/zsh
 }
 
