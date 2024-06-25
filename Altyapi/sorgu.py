953470e0639255737450022e04f0db3f084f52d9
0x733 <98838267+0x733@users.noreply.github.com>
2024-06-26 00:44:44 +0300

    .

--- Altyapi/sorgu.py
+++ Altyapi/sorgu.py
@@ -2,10 +2,11 @@ import requests
 import bs4
 import json
 import time
-from datetime import datetime
+from datetime import datetime, timedelta
 
-bbk = "0000000000"  # buraya goknet.com.tr port sorgu sayfasına adresinizi girince çıkan kodu yapıştırın
+bbk = "0000000000"
 url = f"https://user.goknet.com.tr/sistem/getTTAddressWebservice.php?kod={bbk}&datatype=checkAddress"
+end_time = datetime.now() + timedelta(seconds=10)
 
 def check_port_status():
     try:
@@ -13,11 +14,8 @@ def check_port_status():
         response.raise_for_status()
         soup = bs4.BeautifulSoup(response.text, 'html.parser')
         
-        # JSON verisini konsola yazdır
         json_data = json.loads(str(soup))
-        print(json.dumps(json_data, indent=4, ensure_ascii=False))
 
-        # Beklenen JSON yapısına sahip olup olmadığını kontrol et
         if '6' in json_data and 'flexList' in json_data['6'] and 'flexList' in json_data['6']['flexList'] and len(json_data['6']['flexList']['flexList']) > 2:
             port_value = json_data['6']['flexList']['flexList'][2]['value']
             error_code = json_data['6']['hataKod']
@@ -26,20 +24,30 @@ def check_port_status():
             port_status = 'VAR' if port_value == '1' else 'YOK'
 
             now = datetime.now()
-            current_time = now.strftime("%H:%M:%S")
+            current_time = now.strftime("%Y-%m-%d %H:%M:%S")
 
             log_message = f"[{current_time}] Port Durumu: {port_status}, Hata Kodu: {error_code}, Mesaj: {message}"
             print(log_message)
         else:
-            print("Beklenen JSON yapısı bulunamadı")
+            now = datetime.now()
+            current_time = now.strftime("%Y-%m-%d %H:%M:%S")
+            print(f"[{current_time}] Beklenen JSON yapısı bulunamadı")
 
     except requests.exceptions.RequestException as e:
-        print(f"İstek hatası: {e}")
+        now = datetime.now()
+        current_time = now.strftime("%Y-%m-%d %H:%M:%S")
+        print(f"[{current_time}] İstek hatası: {e}")
     except json.JSONDecodeError as e:
-        print(f"JSON ayrıştırma hatası: {e}")
+        now = datetime.now()
+        current_time = now.strftime("%Y-%m-%d %H:%M:%S")
+        print(f"[{current_time}] JSON ayrıştırma hatası: {e}")
     except Exception as e:
-        print(f"Beklenmeyen hata: {e}")
+        now = datetime.now()
+        current_time = now.strftime("%Y-%m-%d %H:%M:%S")
+        print(f"[{current_time}] Beklenmeyen hata: {e}")
 
-while True:
+while datetime.now() < end_time:
     check_port_status()
     time.sleep(60)
+
+print("Program sonlandı.")
