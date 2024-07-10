c6451867cb0efa3249da7dfe4912bf499ef69d29
0x733 <98838267+0x733@users.noreply.github.com>
2024-07-10 10:03:24 +0300

    ☠️

--- CachyOS/install.sh
+++ CachyOS/install.sh
@@ -3,7 +3,8 @@ set -euo pipefail
 
 sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com || { echo "Error: Chaotic AUR key fetch failed."; exit 1; }
 sudo pacman-key --lsign-key 3056513887B78AEB
-sudo pacman -U --noconfirm 'https://cdn-mirror.chaotic.cx/chaotic-aur/{chaotic-keyring,chaotic-mirrorlist}.pkg.tar.zst' || { echo "Error: Chaotic AUR install failed."; exit 1; }
+sudo pacman -U --noconfirm 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' \
+                            'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst' || { echo "Error: Chaotic AUR install failed."; exit 1; }
 sudo tee -a /etc/pacman.conf << EOF
 [chaotic-aur]
 Include = /etc/pacman.d/chaotic-mirrorlist
