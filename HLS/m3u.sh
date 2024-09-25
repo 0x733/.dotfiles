b969fa69f21d1bcee4eed8cf13b2dea0ad325e65
0x733 <98838267+0x733@users.noreply.github.com>
2024-09-25 09:26:22 +0300

    up

--- HLS/m3u.sh
+++ HLS/m3u.sh
@@ -2,11 +2,12 @@
 OUTPUT_FILE="Filmler.m3u"
 [[ ! -f "$OUTPUT_FILE" ]] && echo "#EXTM3U" > "$OUTPUT_FILE"
 echo "Film adı:"; read film_name
+echo "Film süresi (dakika):"; read film_duration
 echo "Film resmi URL'si:"; read film_logo
 echo "Film video URL'si:"; read film_url
 echo "Referrer URL'si:"; read referrer
 referrer="${referrer%/}/"
-echo -e "\n#EXTINF:-1 tvg-name=\"$film_name\" tvg-language=\"Turkish\" tvg-country=\"TR\" tvg-id=\"$film_name\" tvg-logo=\"$film_logo\" group-title=\"Korku\",$film_name" >> "$OUTPUT_FILE"
+echo -e "\n#EXTINF:$film_duration tvg-name=\"$film_name\" tvg-language=\"Turkish\" tvg-country=\"TR\" tvg-id=\"$film_name\" tvg-logo=\"$film_logo\" group-title=\"Korku\",$film_name" >> "$OUTPUT_FILE"
 echo "#EXTVLCOPT:http-user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5)" >> "$OUTPUT_FILE"
 echo "#EXTVLCOPT:http-referrer=$referrer" >> "$OUTPUT_FILE"
 echo "$film_url" >> "$OUTPUT_FILE"
